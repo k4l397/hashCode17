e3a845478118e9280f89c00bb9d2e74c03a4ccf4
Iwan <iwan.cole.2015@bristol.ac.uk>
2017-02-23 19:35:46 +0000

    Added requests count

--- getInput.py
+++ getInput.py
@@ -1,6 +1,6 @@
 from video import Video
 
-file = open('kittens.in', 'r')
+file = open('me_at_the_zoo.in', 'r')
 metadata = file.readline().strip()
 metadata = metadata.split(' ')
 noOfVideos = int(metadata[0])
@@ -12,9 +12,19 @@ print 'Number of videos %d' % noOfVideos
 print 'Number of endpoints %d' % noOfEndpoints
 print 'Number of requests %d' % noOfRequestDescriptors
 print 'Number of cache servers %d' % noOfCacheServers
-print 'capacity of servers %d' % capacity 
+print 'capacity of servers %d' % capacity
 
 videos = []
 sizes = file.readline().strip().split(' ')
 for x in range(0, noOfVideos):
     videos.append(Video(x, int(sizes[x])))
+
+file.close()
+f = open("me_at_the_zoo.in", "r")
+for line in f:
+    if len(line.split(" ")) == 3:
+        data = [int(x) for x in line.split(" ")]
+        videos[data[0]].setRequests(data[2], data[1])
+
+# print (videos[1].requests)
+# print (videos[1].totalRequests)
--- video.py
+++ video.py
@@ -3,7 +3,9 @@ class Video:
         self.id = id
         self.size = size
         self.requests = []
+        self.totalRequests = 0
 
     def setRequests(self, num, endpointID):
         newEntry = [endpointID, num]
         self.requests.append(newEntry)
+        self.totalRequests += newEntry[1]
