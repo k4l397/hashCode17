80532253e4169884ca737fb2cac27ccf8e0da4be
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-02-23 21:19:38 +0000

    Add addVideo

--- cache.py
+++ cache.py
@@ -5,6 +5,11 @@ class Cache:
 
     def __init__(self, id):
         self.id = id
+        self.currentCapacity = 0
         self.latency = {}
         self.endpoints = {}
         self.videos = []
+    
+    def addVideo(self, video):
+        self.videos.append(video.Id)
+        self.currentCapacity += video.size
