0d336015c8cf74f62b405e165189b589a3bf9f11
Bilal Kazi <bilalahmedkazi@hotmail.com>
2017-02-23 18:41:23 +0000

    update getinput

--- getInput.py
+++ getInput.py
@@ -1,13 +1,13 @@
 file = open('kittens.in', 'r')
 metadata = file.readline().strip()
 metadata = metadata.split(' ')
-noOfVideos = metadata[0]
-noOfEndpoints = metadata[1]
-noOfRequestDescriptors = metadata[2]
-noOfCacheServers = metadata[3]
-capacity = metadata[4]
-print 'Number of videos %s' % noOfVideos
-print '' % noOfEndpoints
+noOfVideos = int(metadata[0])
+noOfEndpoints = int(metadata[1])
+noOfRequestDescriptors = int(metadata[2])
+noOfCacheServers = int(metadata[3])
+capacity = int(metadata[4])
+print 'Number of videos %d' % noOfVideos
+print 'Number of endpoints %d' % noOfEndpoints
 print noOfRequestDescriptors
 print noOfCacheServers
 print capacity  
