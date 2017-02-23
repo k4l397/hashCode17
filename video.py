ea09a31431c8cce52724d32a604bd548cc3f26ed
Iwan <iwan.cole.2015@bristol.ac.uk>
2017-02-23 19:16:23 +0000

    Start request count

--- video.py
+++ video.py
@@ -2,3 +2,8 @@ class Video:
     def __init__(self, id, size):
         self.id = id
         self.size = size
+        self.requests = []
+
+    def setRequests(self, num, endpointID):
+        newEntry = [endpointID, num]
+        self.requests.append(newEntry)
